380db4253ccf948e512031c81c7ad65c3fa5ae6f
Jonathan Elbert Anggawijaya <jonathananggawijaya@gmail.com>
2024-12-26 16:13:13 +0700

    pushing the files

---
+++ untitled/src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package com.tomek1007.chapter4;
+
+import org.datavec.api.split.FileSplit;
+import org.datavec.api.util.ClassPathResource;
+import org.deeplearning4j.api.storage.StatsStorage;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.
